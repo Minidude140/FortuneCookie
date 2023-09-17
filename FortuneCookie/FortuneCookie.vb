d35a45f9309792eaea27a30136c84631ce983cdd
Zac <zac.minidude140@Gmail.com>
2023-09-16 19:16:16 -0600

    Added Main loop and quit

--- FortuneCookie/FortuneCookie.vb
+++ FortuneCookie/FortuneCookie.vb
@@ -11,7 +11,7 @@ Option Strict On
 Module FortuneCookie
 
     'TODO
-    '[]Make main loop: Quit, CLear messages, and Open cookie
+    '[*~]Make main loop: Quit, CLear messages, and Open cookie
     '[]OpenCookie Sub that writes message to the user
     '[]OpenCookie Sub should take random number to determine the response
     '[]Create at least 3 responses
@@ -19,6 +19,23 @@ Module FortuneCookie
 
 
     Sub Main()
+        Dim exitFlag As Boolean = False
+        Dim userInput As String
+
+        Do While exitFlag = False
+            Console.WriteLine("Hello, would you like a fortune cookie?" & vbLf & "(Press any key to Open a Cookie or Press Q to leave)")
+            userInput = Console.ReadLine()
+            Console.WriteLine()
+
+            Select Case userInput
+                Case = "Q", "q"
+                    exitFlag = True
+                Case Else
+                    'open cookie routine
+                    Console.WriteLine("*hands too weak to open cookie*" & vbLf & "You reluctantly return the cookie" & vbLf)
+            End Select
+        Loop
+
 
     End Sub
 
