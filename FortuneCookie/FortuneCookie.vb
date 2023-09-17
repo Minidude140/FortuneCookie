13899c5943c9a2c7b80b1b50e4e8ed32fd591589
Zac <zac.minidude140@Gmail.com>
2023-09-16 19:02:34 -0600

    Added TODO and Git link in header

--- FortuneCookie/FortuneCookie.vb
+++ FortuneCookie/FortuneCookie.vb
@@ -2,13 +2,21 @@
 'RCET 2265
 'Fall 2023
 'Fortune Cookie
-'https://github.com/Minidude140
+'https://github.com/Minidude140/FortuneCookie.git
 
 
 Option Explicit On
 Option Strict On
 
 Module FortuneCookie
+
+    'TODO
+    '[]Make main loop: Quit, CLear messages, and Open cookie
+    '[]OpenCookie Sub that writes message to the user
+    '[]OpenCookie Sub should take random number to determine the response
+    '[]Create at least 3 responses
+
+
 
     Sub Main()
 
